67321aa5f2457e78e2831cd7fea3732e02ce69be
comboldam <plutususapparel@gmail.com>
2025-12-08 12:29:49 -0600

    Add immersive mode for full screen experience

--- android/app/src/main/java/com/quizzena/app/MainActivity.java
+++ android/app/src/main/java/com/quizzena/app/MainActivity.java
@@ -1,5 +1,52 @@
 package com.quizzena.app;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable immersive mode for navigation bar only (keep status bar visible)
+        enableNavigationImmersive();
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableNavigationImmersive();
+        }
+    }
+    
+    private void enableNavigationImmersive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11+ (API 30+)
+            // DON'T use setDecorFitsSystemWindows(false) - this causes content to go behind status bar
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                // Only hide navigation bar, keep status bar
+                controller.hide(WindowInsets.Type.navigationBars());
+                // Swipe to reveal navigation bar
+                controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        } else {
+            // Android 10 and below
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+}
